53f455293dc4d8ed030e8d3d791d9a054f47ce06
nehanvee <nehanvees@gmail.com>
2026-07-07 11:54:50 +0530

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    uint public number;
+
+    function setNumber(uint _number) public {
+        number = _number;
+    }
+
+    function getNumber() public view returns(uint) {
+        return number;
+    }
+
+}
